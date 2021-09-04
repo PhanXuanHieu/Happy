963a9da3c12f7ac9f9a762950f3170c15b46593e
Hiếu <60700486+PhanXuanHieu@users.noreply.github.com>
2021-09-04 21:29:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+
+var config = {
+
+    texts: [
+        "Tuổi mới luôn vui vẻ",
+        "Cuộc sống tràn ngập niềm vui",
+        "Ngày càng xinh đẹp nữa",
+        "Học giỏi hơn nữa này",
+        "Thành công trong cuộc sống",
+        "Hạnh phúc bên gia đình",
+        "Chúc bạn sớm có người yêu",
+        "Có thể không ai nhớ ngày của bạn",
+        "Nhưng mình vẫn nhớ nha:v",
+        "Mình muốn nói là",
+        "CHÚC BẠN LUÔN MAY MẮN VÀ THÀNH CÔNG TRÊN CON ĐƯỜNG ĐÃ CHỌN",
+        "HAPPY BIRTHDAY TO YOU~~",
+    ],
+
+    imgs: {
+        "Tuổi mới luôn vui vẻ": "./imgs/a12.jpg",
+        "Cuộc sống tràn ngập niềm vui": "./imgs/a2.jpg",
+        "Ngày càng xinh đẹp nữa": "./imgs/xiaokeai.png",
+        "Học giỏi hơn nữa này": "./imgs/a5.jpg",
+        "Thành công trong cuộc sống": "./imgs/a7.jpg",
+        "Hạnh phúc bên gia đình": "./imgs/a1.jpg",
+        "Chúc bạn sớm có người yêu": "./imgs/ny.jpg",
+    },
+    desc: {
+        turn_on: "Bắt đầu nào",
+        play: "Bật tý nhạc lên chứ",
+        bannar_coming: "Mang poster ra nào",
+        balloons_flying: "Thêm một tý bóng bay",
+        cake_fadein: "Mang bánh lên nữa chứ",
+        light_candle: "Thắp nến lên nào",
+        wish_message: "HAPPY BIRTHDAY TO YOU",
+        story: "Có những lời chúc cho bạn nè",
+    }
+};
